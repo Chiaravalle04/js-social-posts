61796f1c7984340a8820aca95a11df5a92ce10cc
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-01-18 15:36:30 +0100

    Completata MILESTONE 2

--- main.js
+++ main.js
@@ -59,3 +59,177 @@ const posts = [
         created: "2021-03-05"
     }
 ];
+
+// riferimento al container principale
+const generalContainer = document.getElementById('container');
+
+// creazione posts
+for (let i = 0; i < posts.length; i++) {
+
+    // post container generale
+    let postContainer = document.createElement('div');
+
+    postContainer.classList.add('post');
+
+    generalContainer.append(postContainer);
+
+    //header del post
+    let headerPost = document.createElement('div');
+
+    headerPost.classList.add('post__header');
+
+    postContainer.append(headerPost);
+
+    // Immagine, Nome e Data
+    let postMetaContainer = document.createElement('div');
+
+    postMetaContainer.classList.add('post-meta');
+
+    headerPost.append(postMetaContainer);
+
+    // Container immagine profilo
+    let profileImgContainer = document.createElement('div');
+
+    profileImgContainer.classList.add('post-meta__icon');
+
+    postMetaContainer.append(profileImgContainer);
+
+    // Immagine profilo
+    let profileImg = document.createElement('img');
+
+    profileImg.classList.add('profile-pic');
+
+    profileImgContainer.append(profileImg);
+
+    if (posts[i].author.image == null) {
+
+        posts[i].author.image = 'https://cdn.arstechnica.net/wp-content/uploads/archive/02-16-2011/facebook_head.jpg';
+
+    }
+
+    profileImg.src = posts[i].author.image;
+
+
+    // Container Nome e Data
+    let ndContainer = document.createElement('div');
+
+    ndContainer.classList.add('post-meta__data');
+
+    postMetaContainer.append(ndContainer);
+
+    // Nome autore post
+    let authorPost = document.createElement('div');
+
+    authorPost.classList.add('post-meta__author');
+
+    ndContainer.append(authorPost);
+
+    authorPost.innerHTML = posts[i].author.name;
+
+    // Data del post 
+    let datePost = document.createElement('div');
+
+    datePost.classList.add('post-meta__time');
+
+    ndContainer.append(datePost);
+
+    datePost.innerHTML = posts[i].created;
+
+    // Caption post
+    let caption = document.createElement('div');
+
+    caption.classList.add('post__text');
+
+    postContainer.append(caption);
+
+    caption.innerHTML = posts[i].content;
+
+    // Container immagine post
+    let containerPostImg = document.createElement('div');
+
+    containerPostImg.classList.add('post__image');
+
+    postContainer.append(containerPostImg);
+
+    // Immagine post
+    let postImg = document.createElement('img');
+
+    containerPostImg.append(postImg);
+
+    postImg.src = posts[i].media;
+
+    
+    // Footer post 
+    let footerPost = document.createElement('div');
+
+    footerPost.classList.add('post__footer');
+
+    postContainer.append(footerPost);
+
+    // Likes container generale
+    let likesContainerGeneral = document.createElement('div');
+
+    likesContainerGeneral.classList.add('likes', 'js-likes');
+
+    footerPost.append(likesContainerGeneral);
+
+    // Likes container
+    let likesContainer = document.createElement('div');
+
+    likesContainer.classList.add('likes__cta');
+
+    likesContainerGeneral.append(likesContainer);
+
+    // Link 
+    let link = document.createElement('a');
+
+    link.classList.add('like-button', 'js-like-button');
+
+    link.setAttribute('href', '#');
+
+    link.setAttribute('data-postid', '1');
+
+    likesContainer.append(link);
+
+    // Icon
+    let icon = document.createElement('i');
+
+    icon.classList.add('like-button__icon', 'fas', 'fa-thumbs-up');
+
+    icon.setAttribute('aria-hidden', 'true');
+
+    link.append(icon);
+
+    // Like label
+    let likeLabel = document.createElement('span');
+
+    likeLabel.classList.add('like-button__label');
+
+    link.append(likeLabel);
+
+    likeLabel.innerHTML = 'Mi Piace';
+
+    // Likes counter
+    let likesCounter = document.createElement('div');
+
+    likesCounter.classList.add('likes__counter');
+
+    likesContainerGeneral.append(likesCounter);
+
+    // likes number 
+    let likesNumber = document.createElement('b');
+
+    likesNumber.classList.add('js-likes-counter');
+
+    likesNumber.setAttribute('id', 'like-counter-1');
+
+    likesNumber.innerHTML = posts[i].likes;
+
+    // Likes number label
+    let likesNumberLabel = document.createElement('span');
+
+    likesCounter.append(likesNumberLabel);
+
+    likesNumberLabel.innerHTML = `Piace a ${likesNumber.outerHTML} persone`;
+
+}
